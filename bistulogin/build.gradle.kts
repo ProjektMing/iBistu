6aaca285585755c5f9f877f5d0d0dd4e324ebdd1
Fufuzka <smtxhka@outlook.com>
2026-07-26 00:20:03 +0800

    chore(build): remove vulnerable Foojay resolver

--- bistulogin/build.gradle.kts
+++ bistulogin/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     id("maven-publish")
@@ -5,8 +7,13 @@ plugins {
 
 java {
     withSourcesJar()
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(11)
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
     }
 }
 
